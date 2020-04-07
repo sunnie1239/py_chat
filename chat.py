703b5fdbbc4d6651ba48ae2de617f01202ffac81
sunnie1239 <sunnie1239@gmail.com>
2020-04-06 20:10:48 -0700

    optimize by function

--- chat.py
+++ chat.py
@@ -1,23 +1,33 @@
 import os
 
-filename1 = 'input.txt'
-filename2 = 'output.txt'
-
-if os.path.isfile(filename1):
+# 讀檔並轉換
+def convert(filename):
 	chat = []
 	name = None
 	# 檔案存在
-	with open(filename1, 'r', encoding='utf-8') as f:
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			line = line.strip()
 			if 'Allen' in line or 'Tom' in line:
 				name = line
 			else:
 				if name:
-					chat.append(name + ':' + line)		
-	# 寫入檔案
-	with open(filename2, 'w', encoding='utf-8') as f:
+					chat.append(name + ':' + line)	
+	return chat				
+
+# 寫入檔案
+def write_file(filename, chat):
+	with open(filename, 'w', encoding='utf-8') as f:
 		for line in chat:
 			f.write(line + '\n')
-else:
-	print('檔案不存在')	
+
+# 主程式
+def main():
+	if os.path.isfile('input.txt'):
+		chat = convert('input.txt')
+		write_file('output.txt', chat)
+	else:
+		print('檔案不存在')	
+
+#執行程式
+main()
