de53820d7a72e6899ecc04222fb6ad296a1b8176
sunnie1239 <sunnie1239@gmail.com>
2020-04-06 19:47:30 -0700

    property of none

--- chat.py
+++ chat.py
@@ -5,16 +5,16 @@ filename2 = 'output.txt'
 
 if os.path.isfile(filename1):
 	chat = []
+	name = None
 	# 檔案存在
 	with open(filename1, 'r', encoding='utf-8') as f:
 		for line in f:
 			line = line.strip()
 			if 'Allen' in line or 'Tom' in line:
 				name = line
-				continue
 			else:
-				chat.append(name + ':' + line)	
-	# print(chat)
+				if name:
+					chat.append(name + ':' + line)		
 	# 寫入檔案
 	with open(filename2, 'w', encoding='utf-8') as f:
 		for line in chat:
